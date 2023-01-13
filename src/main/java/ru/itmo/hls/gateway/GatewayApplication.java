c20067647528c681eae3aab9b67d2869f27de585
Кирилл Шахов <kirill.shakhov14@gmail.com>
2023-01-14 00:37:58 +0700

    feat: gateway service

---
+++ src/main/java/ru/itmo/hls/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package ru.itmo.hls.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+@EnableFeignClients
+@EnableEurekaClient
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
