6d538e8d684022954a5786cda9e19aaa4df69800
Кирилл Шахов <kirill.shakhov14@gmail.com>
2023-01-14 06:41:24 +0700

    fix(authorization): controller

--- src/main/java/ru/itmo/hls/lab1/authorization/AuthorizationApplication.java
+++ src/main/java/ru/itmo/hls/lab1/authorization/AuthorizationApplication.java
@@ -2,8 +2,13 @@ package ru.itmo.hls.lab1.authorization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableHystrix
+@EnableDiscoveryClient
 public class AuthorizationApplication {
 
 	public static void main(String[] args) {
